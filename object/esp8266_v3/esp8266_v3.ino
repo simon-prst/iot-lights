f7df7ebbc78aa35d3e3430d017ef21ab02e1ba5c
Simon <parisot.simon@gmail.com>
2018-02-26 00:09:22 +0100

    new hardware v3

--- object/esp8266_v3/esp8266_v3.ino
+++ object/esp8266_v3/esp8266_v3.ino
@@ -17,6 +17,8 @@ const char* outTopic = "state";
 const char* inTopic = "command";
 int relayPin = 2;                                       // pin that commands the relay
 int buttonPin = 0;                                       // pin for the physical switch
+int buttonPreviousState = HIGH;
+int relayState;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -56,15 +58,22 @@ void callback(char* topic, byte* payload, unsigned int length) {
   }
   Serial.println();
 
-  // Toggle the relay if an 1 was received as first character
-  if ((char)payload[0] == 'a') { 
+  // Toggle the relay if 2 was received as first characters
+  if ((char)payload[0] == '2') { 
+    Serial.println("Command received from the internet!");
     toggleRelay();
   }
 }
 
 void toggleRelay(){
-  Serial.println("Toggle the relay");
-  digitalWrite(relayPin, !digitalRead(relayPin));
+  // Serial.println("Toggle the relay");
+  relayState = digitalRead(relayPin);
+  digitalWrite(relayPin, !relayState);
+  if (!relayState) {
+    client.publish(outTopic, "Relay is on");
+  }else{
+    client.publish(outTopic, "Relay is off");
+  }
 }
 
 void reconnect() {
@@ -95,7 +104,7 @@ void setup() {
   pinMode(relayPin, OUTPUT);     // Initialize the pin which commands the relay
   pinMode(buttonPin, INPUT);     // Initialise the input for physical switch
   digitalWrite(buttonPin, HIGH); 
-  Serial.begin(115200);
+  Serial.begin(74880);
   setup_wifi();
   client.setServer(mqtt_server, mqtt_port);
   client.setCallback(callback);
@@ -108,7 +117,10 @@ void loop() {
   }
   client.loop();
   
-  if (digitalRead(buttonPin) == LOW) { 
+  if (digitalRead(buttonPin) != buttonPreviousState) { 
+    buttonPreviousState = digitalRead(buttonPin);
     toggleRelay();
+    Serial.println("Command received from home switch!");
+    delay(400);
   }
 }
